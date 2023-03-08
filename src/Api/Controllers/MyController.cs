62c1a9838934d5a73b14de310e378e91c9790617
Ali M <ali.mo.dev@gmail.com>
2023-03-08 14:56:36 +0330

    implemented simple rate limiting

---
+++ src/Api/Controllers/MyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/controller")]
+public class MyController : ControllerBase
+{
+    [HttpGet("resource")]
+    [EnableRateLimiting("fixed")]
+    public async Task<IActionResult> LimitedResource()
+    {
+        await Task.Delay(1000);
+
+        var random = new Random();
+
+        return Ok(random.Next(1, 101));
+    }
+
+    [HttpGet("resource2")]
+    [DisableRateLimiting]
+    public async Task<IActionResult> NotLimitedResource()
+    {
+        await Task.Delay(1000);
+
+        var random = new Random();
+
+        return Ok(random.Next(1, 101));
+    }
+}
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,56 @@
+using System.Globalization;
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRateLimiter(config =>
+{
+
+    config.OnRejected = (context, _) =>
+    {
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+        {
+            context.HttpContext.Response.Headers.RetryAfter =
+                ((int)retryAfter.TotalSeconds).ToString(NumberFormatInfo.InvariantInfo);
+        }
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+        context.HttpContext.Response.WriteAsync("Too many requests. Please try again later.");
+
+        return new ValueTask();
+    };
+
+    config.AddFixedWindowLimiter(policyName: "fixed", options =>
+    {
+        options.PermitLimit = 2;
+        options.Window = TimeSpan.FromSeconds(15);
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        options.QueueLimit = 0;
+    });
+});
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRateLimiter();
+
+app.MapGet("/api/resource", () =>
+{
+    var random = new Random();
+
+    return Results.Ok(random.Next(1, 101));
+}).RequireRateLimiting("fixed");
+
+// No Limit
+app.MapGet("/api/resource2", () =>
+{
+    var random = new Random();
+
+    return Results.Ok(random.Next(1, 101));
+}).DisableRateLimiting();
+
+app.MapControllers();
 
 app.Run();
